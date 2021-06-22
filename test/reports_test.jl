a333ab0b04b1bc4ad6e9403d7aba09b27ecc7328
Alinson S Xavier <axavier@anl.gov>
2021-06-22 10:58:46 -0500

    Remove unused code and test resources

--- test/reports_test.jl
+++ test/reports_test.jl
@@ -4,30 +4,7 @@
 
 using RELOG, JSON, GZip
 
-load_json_gz(filename) = JSON.parse(GZip.gzopen(filename))
-
-# function check(func, expected_csv_filename::String)
-#     solution = load_json_gz("fixtures/nimh_solution.json.gz")
-#     actual_csv_filename = tempname()
-#     func(solution, actual_csv_filename)
-#     @test isfile(actual_csv_filename)
-#     if readlines(actual_csv_filename) != readlines(expected_csv_filename)
-#         out_filename = replace(expected_csv_filename, ".csv" => "_actual.csv")
-#         @error "$func: Unexpected CSV contents: $out_filename"
-#         write(out_filename, read(actual_csv_filename))
-#         @test false
-#     end
-# end
-
 @testset "Reports" begin
-    #     @testset "from fixture" begin
-    #         check(RELOG.write_plants_report, "fixtures/nimh_plants.csv")
-    #         check(RELOG.write_plant_outputs_report, "fixtures/nimh_plant_outputs.csv")
-    #         check(RELOG.write_plant_emissions_report, "fixtures/nimh_plant_emissions.csv")
-    #         check(RELOG.write_transportation_report, "fixtures/nimh_transportation.csv")
-    #         check(RELOG.write_transportation_emissions_report, "fixtures/nimh_transportation_emissions.csv")
-    #     end
-
     @testset "from solve" begin
         solution = RELOG.solve("$(pwd())/../instances/s1.json")
         tmp_filename = tempname()
