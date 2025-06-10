5ea3e10139afdf0fc6e721a000dc5bc29a975ac4
Alinson S. Xavier <git@axavier.org>
2025-06-10 09:58:39 -0500

    web: Update copyright

--- web/src/components/Common/Forms/Toast.module.css
+++ web/src/components/Common/Forms/Toast.module.css
@@ -1,5 +1,5 @@
 /*
- * UnitCommitment.jl: Optimization Package for Security-Constrained Unit Commitment
+ * RELOG: Supply Chain Analysis and Optimization
  * Copyright (C) 2020-2025, UChicago Argonne, LLC. All rights reserved.
  * Released under the modified BSD license. See COPYING.md for more details.
  */
